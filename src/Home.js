9ca91fbda3ebef636ac685f19ca399b9ef5120b6
HAWK <salirizvi30@gmail.com>
2022-10-06 08:45:40 -0700

    URL ISSUE

--- src/Home.js
+++ src/Home.js
@@ -5,7 +5,7 @@ import BlogList from "./BlogList";
 const Home = () => {
   const dotEnv = process.env.NODE_ENV !== "production";
   const {REACT_APP_DEV_URL,REACT_APP_PROD_URL} = process.env; 
-  const {data:blogs,loading,error} = useFetch(`${dotEnv?REACT_APP_DEV_URL:REACT_APP_PROD_URL}`)
+  const {data:blogs,loading,error} = useFetch('http://hawkblogsapp.herokuapp.com/blogs')
   return (
     <div className="home">
         {error && <div><h1>{error}</h1></div>}
